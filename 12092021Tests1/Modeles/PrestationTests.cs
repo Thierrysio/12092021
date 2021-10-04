0c5ad2d382233b11e113fedfa8dec6f1bb6634da
thierry le gall <thierry28220@live.fr>
2021-10-04 11:24:57 +0200

    ok

--- 12092021Tests1/Modeles/PrestationTests.cs
+++ 12092021Tests1/Modeles/PrestationTests.cs
@@ -21,15 +21,51 @@ namespace _12092021.Modeles.Tests
             Prestation P1 = new Prestation(5, TP1);
 
             Activite A1 = new Activite("Activite 01");
-            Activite A1 = new Activite("Activite 01");
-            Activite A1 = new Activite("Activite 01");
-            Activite A1 = new Activite("Activite 01");
-            Activite A1 = new Activite("Activite 01");
+            Activite A2 = new Activite("Activite 02");
+            Activite A3 = new Activite("Activite 03");
+            Activite A4 = new Activite("Activite 04");
+            Activite A5 = new Activite("Activite 05");
 
+            P1.AjoutAuDicoNbreActivites(A1, 1);
+            P1.AjoutAuDicoNbreActivites(A2, 1);
+            P1.AjoutAuDicoNbreActivites(A1, 1);
+
+            Tache T1 = new Tache("Tache 01");
+            Tache T2 = new Tache("Tache 02");
+            Tache T3 = new Tache("Tache 03");
+            Tache T4 = new Tache("Tache 04");
+            Tache T5 = new Tache("Tache 05");
+
+            A1.AjoutAuDicoNbreTaches(T1, 1);
+            A1.AjoutAuDicoNbreTaches(T3, 1);
+            A1.AjoutAuDicoNbreTaches(T1, 1);
+            A2.AjoutAuDicoNbreTaches(T1, 1);
+            A2.AjoutAuDicoNbreTaches(T5, 1);
+
+            MicroTache MT1 = new MicroTache("Microtache 01");
+            MicroTache MT2 = new MicroTache("Microtache 02");
+            MicroTache MT3 = new MicroTache("Microtache 03");
+            MicroTache MT4 = new MicroTache("Microtache 04");
+            MicroTache MT5 = new MicroTache("Microtache 05");
+
+            T1.AjoutDicoNbreMicroTaches(MT1, 1);
+            T1.AjoutDicoNbreMicroTaches(MT3, 1);
+            T1.AjoutDicoNbreMicroTaches(MT1, 1);
+
+            
+            T3.AjoutDicoNbreMicroTaches(MT5, 1);
+            T3.AjoutDicoNbreMicroTaches(MT2, 1);
+
+            T5.AjoutDicoNbreMicroTaches(MT1, 1);
+            T5.AjoutDicoNbreMicroTaches(MT2, 1);
 
             // Methodes à tester
 
+            Dictionary<MicroTache, int> expected = P1.GetListeMicroTaches();
+
             // Assert
+
+            Assert.AreEqual(1, 1);
         }
     }
 }
