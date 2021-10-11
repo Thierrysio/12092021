82282b4c88d0a1eca281f2d80a1ee1c2ec6817f5
thierry le gall <thierry28220@live.fr>
2021-10-11 11:03:08 +0200

    ok

--- 12092021/Modeles/Appartement.cs
+++ 12092021/Modeles/Appartement.cs
@@ -12,6 +12,7 @@ namespace _12092021.Modeles
 
         public static List<Appartement> CollClasse = new List<Appartement>();
         private string _nom;
+        private Dictionary<Semaine, Locataire> _dicoLocation;
         #endregion
 
         #region Constructeurs
@@ -20,17 +21,63 @@ namespace _12092021.Modeles
         {
             Appartement.CollClasse.Add(this);
             _nom = nom;
+            _dicoLocation = new Dictionary<Semaine, Locataire>();
         }
 
         #endregion
 
         #region Getters/Setters
         public string Nom { get => _nom; set => _nom = value; }
+        public Dictionary<Semaine, Locataire> DicoLocation { get => _dicoLocation; set => _dicoLocation = value; }
 
         #endregion
 
         #region Methodes
 
+        public Locataire QuiALoue(Semaine param)
+        {
+            Locataire resultat = null;
+
+            foreach(var pair in this.DicoLocation)
+            {
+                if (pair.Key == param)
+                {
+                    return pair.Value;
+                }
+            }
+
+            return resultat;
+        }
+
+        public bool AjoutLocationAppartement(Semaine param, Locataire param2)
+        {
+            bool resultat = false;
+            // Est ce que l'appartement est libre cette semaine ?
+            // Ajout de l'appartement dans le dico.
+
+            if (this.QuiALoue(param) == null)
+            {
+                this.DicoLocation.Add(param, param2);
+            }
+            else
+            {
+                resultat = true;
+            }
+
+            return resultat;
+        }
+        public bool AjoutLocationAppartement(Locataire param)
+        {
+            bool resultat = false;
+
+            return resultat;
+        }
+        public bool AjoutLocationAppartement()
+        {
+            bool resultat = false;
+
+            return resultat;
+        }
         #endregion
     }
 }
